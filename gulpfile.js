2809d4d53153f0f6a448bfa2e33f23a88b4777d1
Steve Cochrane <steve@stevecochrane.com>
2014-12-27 12:00:12 -0500

    Replaced the old gruntfile with a gulpfile, replaced old Sass with Less. The 'gulp css' workflow is working again, others still need work.

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require("gulp");
+
+var autoprefixer = require("autoprefixer-core"),
+    combineMediaQueries = require("gulp-combine-media-queries"),
+    concat = require("gulp-concat"),
+    imagemin = require("gulp-imagemin"),
+    jshint = require("gulp-jshint"),
+    less = require("gulp-less"),
+    minifyCss = require("gulp-minify-css"),
+    minifyHtml = require("gulp-minify-html"),
+    pixrem = require("gulp-pixrem"),
+    postCss = require("gulp-postcss"),
+    uglify = require("gulp-uglify");
+
+gulp.task("copy-assets", function() {
+    gulp.src("./node_modules/normalize.css/normalize.css")
+        .pipe(gulp.dest("./src/css"));
+});
+
+gulp.task("imagemin", function() {
+    gulp.src("./src/img/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/img/"));
+});
+
+gulp.task("css", function() {
+    gulp.src("./src/less/main.less")
+        .pipe(less())
+        .pipe(pixrem())
+        .pipe(postCss([ autoprefixer() ]))
+        .pipe(combineMediaQueries())
+        .pipe(minifyCss())
+        .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("js", function() {
+    gulp.src("./src/js/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"))
+        .pipe(jshint.reporter("fail"));
+
+    gulp.src(["./src/js/libs/jquery-1.7.2.min.js", "./src/js/main.js"])
+        .pipe(concat("main.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/js/"));
+});
+
+gulp.task("html", function() {
+    gulp.src("./src/**/*.{html,php}")
+        .pipe(minifyHtml())
+        .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("default", ["css", "js", "html"]);
